d6ac1aed8a41f5c4126f61061f39a7010a6957df
Mayccoll <mayccoll@gmail.com>
2014-12-02 18:04:14 -0500

    install all

---
+++ install.all.sh
@@ -0,0 +1,32 @@
+# | Basics | #
+wget -O xt  http://git.io/Dncnzw && chmod +x xt && ./xt && rm xt
+
+# | Eessentials | #
+wget -O xt  http://git.io/-BDg8Q && chmod +x xt && ./xt && rm xt
+
+# | Customize | #
+wget -O xt  http://git.io/h9dVjw && chmod +x xt && ./xt && rm xt
+
+# | Tools | #
+wget -O xt  http://git.io/8yxL5Q && chmod +x xt && ./xt && rm xt
+
+# | Virtual tools | #
+wget -O xt  http://git.io/KnwTFw && chmod +x xt && ./xt && rm xt
+
+# | Extras | #
+wget -O xt  http://git.io/OBOaLQ && chmod +x xt && ./xt && rm xt
+
+# Python
+wget -O xt  http://git.io/S63sLQ && chmod +x xt && ./xt && rm xt
+
+# Ruby
+wget -O xt  http://git.io/ZFV0PA && chmod +x xt && ./xt && rm xt
+
+# Nodejs
+wget -O xt  http://git.io/R7LgJg && chmod +x xt && ./xt && rm xt
+
+# rMate
+wget -O xt  http://git.io/gE58Qg && chmod +x xt && ./xt && rm xt
+
+# zsh
+wget -O xt  http://git.io/UBJMHQ && chmod +x xt && ./xt && rm xt
