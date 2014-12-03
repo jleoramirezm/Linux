99551070e4b478cf402ba1a23ebbee146418d5d2
Mayccoll <mayccoll@gmail.com>
2014-12-03 11:22:22 -0500

    update install.zsh new refact

--- install.zsh.sh
+++ install.zsh.sh
@@ -15,16 +15,34 @@ PURPLE="\e[0;35m"
 CYAN="\e[0;36m"
 WHITE="\e[0;37m"
 
-    # - 
+
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+    # -
     # ==============================================
-echo -e "${YELLOW}Do you have ${RED}root ${YELLOW}permission ?${RS}"
-echo "Yes -> [1]"
-echo "No  -> [2]"
-read PRESSKEY
-if [[ $PRESSKEY -eq 1 ]] || [[ $PRESSKEY -eq "" ]]; then
 sudo apt-get update
+
+
+    # - **Install zsh
+    # ==============================================
 sudo apt-get install -y zsh
+
+
+    # - **Check if command
+    # ==============================================
+git --version 2>&1 >/dev/null
+CHECK=$?
+
+if [ $CHECK -ne 0 ]; then
+    # code..
 fi
+
+
+
+    # - **Setting ZSH as the default shell (instead of bash)**
+    # ==============================================
+w=`which zsh` && h=`whoami` && sudo chsh -s $w $h
 
 
 
@@ -34,76 +52,10 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 
 
 
-    # Delete text beetwen 2 words
+    # - **Install zsh - Add**
     # ==============================================
-FILEZSH="/.bash_profile"
-WORD1='# |::::::::::::::::::>>>proftv' # |<=== Config This
-WORD2='# |::::::::::::::::::<<<proftv' # |<=== Config This
-
-sed -e "/${WORD1}/,/${WORD2}/d"  "${HOME}${FILEZSH}" > "${HOME}${FILEZSH}_tmp"
-cp "${HOME}${FILEZSH}_tmp" "${HOME}${FILEZSH}"
-rm "${HOME}${FILEZSH}_tmp"
-
-
-    # - **Setting ZSH as the default shell (instead of bash)**
-    # ==============================================
-
-if [[ $EUID -ne 0 ]]; then
-
-# no root
-cat <<-EOF >> $HOME/.bash_profile
-# |::::::::::::::::::>>>proftv
-export SHELL=/bin/zsh
-exec /bin/zsh -l
-# |::::::::::::::::::<<<proftv
-EOF
-
-    # Delete text beetwen 2 words
-    # ==============================================
-FILEZSH="/.zshrc"
-WORD1='# |::::::::::::::::::>>>ohmyzsh' # |<=== Config This
-WORD2='# |::::::::::::::::::<<<ohmyzsh' # |<=== Config This
-
-sed -e "/${WORD1}/,/${WORD2}/d"  "${HOME}${FILEZSH}" > "${HOME}${FILEZSH}_tmp"
-cp "${HOME}${FILEZSH}_tmp" "${HOME}${FILEZSH}"
-rm "${HOME}${FILEZSH}_tmp"
-
-
-    # - **Install antigen - Add**
-    # ==============================================
-cat <<-EOF >> $HOME/.zshrc
-# |::::::::::::::::::>>>ohmyzsh
-
-# Path to your oh-my-zsh installation.
-export ZSH=\$HOME/.oh-my-zsh
-
-ZSH_THEME="robbyrussell"
-
-plugins=(git)
-
-source \$ZSH/oh-my-zsh.sh
-
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-
-# |::::::::::::::::::<<<ohmyzsh
-EOF
-
-else
-
-# root
-w=`which zsh` &&  h=`whoami` && sudo chsh -s $w $h
-
-fi
-
-
-
-    # - 
-    # ==============================================
-echo -e "${YELLOW}Do you want install ${red}antigen ${YELLOW}?${RS}"
-echo "Yes -> [1]"
-echo "No  -> [2]"
-read PRESSKEY
-if [[ $PRESSKEY -eq 1 ]] || [[ $PRESSKEY -eq "" ]]; then
+cat $SCRIPT_PATH/files/oh-my-zsh > "${HOME}/.zshrc"
+source "${HOME}/.zshrc"
 
 
     # - **Install antigen**
@@ -111,129 +63,23 @@ if [[ $PRESSKEY -eq 1 ]] || [[ $PRESSKEY -eq "" ]]; then
 cd ~ && git clone https://github.com/zsh-users/antigen.git .antigen
 
 
-    # Delete Content
-echo '' > $HOME/.zshrc
-
-
     # Delete text beetwen 2 words
     # ==============================================
-FILEZSH="/.zshrc"
-WORD1='# |::::::::::::::::::>>>ohmyzsh' # |<=== Config This
-WORD2='# |::::::::::::::::::<<<ohmyzsh' # |<=== Config This
+FILEDEL="${HOME}/.zshrc"
+WORD1='# |::::::::::::::::::>>>antigen' # |<=== Config This
+WORD2='# |::::::::::::::::::<<<antigen' # |<=== Config This
 
-sed -e "/${WORD1}/,/${WORD2}/d"  "${HOME}${FILEZSH}" > "${HOME}${FILEZSH}_tmp"
-cp "${HOME}${FILEZSH}_tmp" "${HOME}${FILEZSH}"
-rm "${HOME}${FILEZSH}_tmp"
+sed -e "/${WORD1}/,/${WORD2}/d"  "${FILEDEL}" > "${FILEDEL}_tmp"
+cp "${FILEDEL}_tmp" "${FILEDEL}"
+rm "${FILEDEL}_tmp"
 
 
     # - **Install antigen - Add**
     # ==============================================
-cat <<-EOF >> $HOME/.zshrc
-# |::::::::::::::::::>>>ohmyzsh
+cat >> $HOME/.zshrc << "EOF"
 
-# Path to your oh-my-zsh installation.
-export ZSH=\$HOME/.oh-my-zsh
-
-# Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
-# Optionally, if you set this to "random", it'll load a random theme each
-# time that oh-my-zsh is loaded.
-ZSH_THEME="robbyrussell"
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
-
-source \$ZSH/oh-my-zsh.sh
-
-# User configuration
-
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/dsa_id"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-# |::::::::::::::::::<<<ohmyzsh
-EOF
-
-
-
-
-
-    # Delete text beetwen 2 words
-    # ==============================================
-FILEZSH="/.zshrc"
-WORD1='# |::::::::::::::::::>>>zsh' # |<=== Config This
-WORD2='# |::::::::::::::::::<<<zsh' # |<=== Config This
-
-sed -e "/${WORD1}/,/${WORD2}/d"  "${HOME}${FILEZSH}" > "${HOME}${FILEZSH}_tmp"
-cp "${HOME}${FILEZSH}_tmp" "${HOME}${FILEZSH}"
-rm "${HOME}${FILEZSH}_tmp"
-
-
-    # - **Install antigen - Add**
-    # ==============================================
-cat <<-EOF >> $HOME/.zshrc
-# |::::::::::::::::::>>>zsh
-source "$HOME/.antigen/antigen.zsh"
+# |::::::::::::::::::>>>antigen
+source "/home/uu/.antigen/antigen.zsh"
 
 # antigen bundle command-not-found
 # antigen bundle fabric
@@ -242,7 +88,7 @@ source "$HOME/.antigen/antigen.zsh"
 # antigen bundle lein
 # antigen bundle pip
 # antigen bundle rake
-# antigen bundle rvme
+# antigen bundle rvm
 # antigen bundle sprunge
 # antigen bundle vundle
 
@@ -259,18 +105,17 @@ antigen-bundle arialdomartini/oh-my-git
 antigen-bundle zsh-users/zsh-history-substring-search
 antigen-bundle zsh-users/zsh-syntax-highlighting
 
-antigen theme Mayccoll/oh-my-git-themes dolar-arrow-green
+antigen theme Mayccoll/oh-my-git-themes liino
 
 antigen-apply
-# |::::::::::::::::::<<<zsh
+# |::::::::::::::::::<<<antigen
 EOF
 
-fi
 
 
     # Delete text beetwen 2 words
     # ==============================================
-FILEZSH="/.zshrc"
+FILEZSH="${HOME}/.zshrc"
 WORD1='# |::::::::::::::::::stars>>>' # |<=== Config This
 WORD2='# |::::::::::::::::::stars<<<' # |<=== Config This
 
@@ -281,12 +126,13 @@ rm "${HOME}${FILEZSH}_tmp"
 
     # - **Install antigen - Add**
     # ==============================================
-cat <<-EOF >> $HOME/.zshrc
+cat >> $HOME/.zshrc << "EOF"
+
 # |::::::::::::::::::stars>>>
 echo '
 　 ･ ·̩　　 ｡　☆　　　ﾟ｡  ＊ 　 ｡*　　+　 　＊ 　･ ｡☆+　 　＊ 　･ ｡☆
 ＊ 　 ｡*　　+　 　＊ 　･ ｡☆  　　　ﾟ･　　｡ﾟ･　　☆ﾟ　+  ｡　☆　　　ﾟ｡･ ·̩　　
-　☆　　　*　　　* 　 。  　 ･ ·̩　　 ｡　☆　　　ﾟ｡　☆　　　*　　　* 　　☆ﾟ　+ 
+　☆　　　*　　　* 　 。  　 ･ ·̩　　 ｡　☆　　　ﾟ｡　☆　　　*　　　* 　　☆ﾟ　+
 　　　ﾟ･　　｡ﾟ･　　☆ﾟ　+ 　☆　　　*　　　* 　 。＊ 　 ｡*　　+　 　＊ 　･ ｡☆
 '
 # |::::::::::::::::::stars<<<
