ca5a314f2843ba9afc9896584abe22f356b43417
Mayccoll <Mayccoll@users.noreply.github.com>
2014-12-02 22:55:10 -0500

    add wait to all

--- install.all.sh
+++ install.all.sh
@@ -1,32 +1,43 @@
 # | Basics | #
 wget -O xt  http://git.io/Dncnzw && chmod +x xt && ./xt && rm xt
+wait
 
 # | Eessentials | #
 wget -O xt  http://git.io/-BDg8Q && chmod +x xt && ./xt && rm xt
+wait
 
 # | Customize | #
 wget -O xt  http://git.io/h9dVjw && chmod +x xt && ./xt && rm xt
+wait
 
 # | Tools | #
 wget -O xt  http://git.io/8yxL5Q && chmod +x xt && ./xt && rm xt
+wait
 
 # | Virtual tools | #
 wget -O xt  http://git.io/KnwTFw && chmod +x xt && ./xt && rm xt
+wait
 
 # | Extras | #
 wget -O xt  http://git.io/OBOaLQ && chmod +x xt && ./xt && rm xt
+wait
 
 # Python
 wget -O xt  http://git.io/S63sLQ && chmod +x xt && ./xt && rm xt
+wait
 
 # Ruby
 wget -O xt  http://git.io/ZFV0PA && chmod +x xt && ./xt && rm xt
+wait
 
 # Nodejs
 wget -O xt  http://git.io/R7LgJg && chmod +x xt && ./xt && rm xt
+wait
 
 # rMate
 wget -O xt  http://git.io/gE58Qg && chmod +x xt && ./xt && rm xt
+wait
 
 # zsh
 wget -O xt  http://git.io/UBJMHQ && chmod +x xt && ./xt && rm xt
+wait
