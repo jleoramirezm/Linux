058095f54d6875964da6732cc0ff34a4af978056
Mayccoll <mayccoll@gmail.com>
2015-02-14 10:54:07 -0500

    update install all

--- install.all.sh
+++ install.all.sh
@@ -24,58 +24,47 @@ function_message_title () {
   echo -e "${RS}"
 }
 
-
 # | Basics | #
 function_message_title '# | Basics | #'
-wget -O xt  http://git.io/Dncnzw && chmod +x xt && ./xt && rm xt
-wait
-
-# | Eessentials | #
-function_message_title '# | Eessentials | #'
-wget -O xy  http://git.io/-BDg8Q && chmod +x xy && ./xy && rm xy
-wait
-
-# | Customize | #
-function_message_title '# | Customize | #'
-wget -O xt  http://git.io/h9dVjw && chmod +x xt && ./xt && rm xt
-wait
-
-# | Tools | #
-function_message_title '# | Tools | #'
-wget -O xt  http://git.io/8yxL5Q && chmod +x xt && ./xt && rm xt
-wait
-
-# | Virtual tools | #
-function_message_title '# | Virtual tools | #'
-wget -O xt  http://git.io/KnwTFw && chmod +x xt && ./xt && rm xt
-wait
-
-# | Extras | #
-function_message_title '# | Extras | #'
-wget -O xt  http://git.io/OBOaLQ && chmod +x xt && ./xt && rm xt
-wait
-
-# | Python | #
-function_message_title '# | Python | #'
-wget -O xt  http://git.io/S63sLQ && chmod +x xt && ./xt && rm xt
-wait
-
-# | Ruby | #
-function_message_title '# | Ruby | #'
-wget -O xt  http://git.io/ZFV0PA && chmod +x xt && ./xt && rm xt
-wait
-
-# | Nodejs | #
-function_message_title '# | Nodejs | #'
-wget -O xt  http://git.io/R7LgJg && chmod +x xt && ./xt && rm xt
-wait
-
-# | rMate | #
-function_message_title '# | rMate | #'
-wget -O xt  http://git.io/gE58Qg && chmod +x xt && ./xt && rm xt
+$ wget -O xt  http://git.io/Dncnzw && chmod +x xt && ./xt && rm xt
 wait
 
 # | zsh | #
 function_message_title '# | zsh | #'
-wget -O xt  http://git.io/UBJMHQ && chmod +x xt && ./xt && rm xt
+$ wget -O xt  http://git.io/UBJMHQ && chmod +x xt && ./xt && rm xt
+wait
+
+# | Eessentials | #
+function_message_title '# | Eessentials | #'
+$ wget -O xt  http://git.io/-BDg8Q && chmod +x xt && ./xt && rm xt
+wait
+
+# | Extras | #
+function_message_title '# | Extras | #'
+$ wget -O xt  http://git.io/OBOaLQ && chmod +x xt && ./xt && rm xt
+wait
+
+# | Design | #
+function_message_title '# | Design | #'
+$ wget -O xt  http://git.io/Nov7 && chmod +x xt && ./xt && rm xt
+wait
+
+# | Customize | #
+function_message_title '# | Customize | #'
+$ wget -O xt  http://git.io/h9dVjw && chmod +x xt && ./xt && rm xt
+wait
+
+# | Tools | #
+function_message_title '# | Tools | #'
+$ wget -O xt  http://git.io/8yxL5Q && chmod +x xt && ./xt && rm xt
+wait
+
+# | Virtual tools | #
+function_message_title '# | Virtual tools | #'
+$ wget -O xt  http://git.io/KnwTFw && chmod +x xt && ./xt && rm xt
+wait
+
+# | Nodejs | #
+function_message_title '# | Nodejs | #'
+$ wget -O xt  http://git.io/R7LgJg && chmod +x xt && ./xt && rm xt
 wait
