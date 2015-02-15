be376525320d746bf7f82fc11d7374ef7f6089ac
Mayccoll <mayccoll@gmail.com>
2015-02-15 17:38:53 -0500

    add font awsome

--- install.zsh.sh
+++ install.zsh.sh
@@ -220,6 +220,21 @@ EOF
 # ======<
 
 
+# - **Install Fonts**
+# ==============================================
+
+# Copy the awesome fonts to ~/.fonts
+cd /tmp
+git clone http://github.com/gabrielelana/awesome-terminal-fonts
+cd awesome-terminal-fonts
+git checkout patching-strategy
+mkdir -p ~/.fonts
+cp patched/*.ttf ~/.fonts
+
+# update the font-info cache
+sudo fc-cache -fv ~/.fonts
+
+
 # - **Install Stars**
 # ==============================================
 
