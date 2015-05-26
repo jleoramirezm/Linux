ce88e8f2c22605187f72347e7cbbf488e86f7286
Mayccoll <mayccoll@gmail.com>
2015-05-26 01:27:55 -0500

    Install stars

--- install.stars.sh
+++ install.stars.sh
@@ -23,29 +23,35 @@ function_message_title () {
   echo -e "${RS}"
 }
 
+
+function_delete_beetwen () {
+  eval XXXXXBEGIN="$1"
+  eval XXXXXXXEND="$2"
+  eval FILE_ZSH="$3"
+  sed -e "/${XXXXXBEGIN}/,/${XXXXXXXEND}/d"  "${FILE_ZSH}" > "${FILE_ZSH}_tmp"
+  cp "${FILE_ZSH}_tmp" "${FILE_ZSH}"
+  rm "${FILE_ZSH}_tmp"
+}
+
+
+
+# - **Install Stars**
+# ==============================================
+
 function_message_title '- Stars -'
+echo "¿ Want to add stars ?"
+echo "Si -> [1]"
+echo "No -> [2]"
+read varpush
+if [[ $varpush -eq 1 ]] || [[ $varpush -eq "" ]]
+  then
 
-    # Delete text beetwen 2 words
-    # ==============================================
-FILEDEL="${HOME}/.zshrc"
-WORD1='# |::::::::::::::::::>>>stars' # |<=== Config This
-WORD2='# |::::::::::::::::::<<<stars' # |<=== Config This
+FILE_ZSH="${HOME}/.zshrc"
+XXXXXBEGIN='# |::::::::::::: stars ::::::::::::::>>>' # |<=== Config This
+XXXXXXXEND='# |::::::::::::: stars ::::::::::::::<<<' # |<=== Config This
+function_delete_beetwen "\${XXXXXBEGIN}" "\${XXXXXXXEND}" "\${FILE_ZSH}"
+echo "${XXXXXBEGIN}" >> $FILE_ZSH
+wget -qO- https://raw.githubusercontent.com/Mayccoll/Linux/master/files/stars.zshrc >> $FILE_ZSH
+echo "${XXXXXXXEND}" >> $FILE_ZSH
 
-sed -e "/${WORD1}/,/${WORD2}/d"  "${FILEDEL}" > "${FILEDEL}_tmp"
-cp "${FILEDEL}_tmp" "${FILEDEL}"
-rm "${FILEDEL}_tmp"
-
-
-    # - **Install Stars - Add**
-    # ==============================================
-cat >> $HOME/.zshrc << "EOF"
-
-# |::::::::::::::::::>>>stars
-echo '
-　 ･ ·̩　　 ｡　☆　　　ﾟ｡  ＊ 　 ｡*　　+　 　＊ 　･ ｡☆+　 　＊ 　･ ｡☆
-＊ 　 ｡*　　+　 　＊ 　･ ｡☆  　　　ﾟ･　　｡ﾟ･　　☆ﾟ　+  ｡　☆　　　ﾟ｡･ ·̩　　
-　☆　　　*　　　* 　 。  　 ･ ·̩　　 ｡　☆　　　ﾟ｡　☆　　　*　　　* 　　☆ﾟ　+
-　　　ﾟ･　　｡ﾟ･　　☆ﾟ　+ 　☆　　　*　　　* 　 。＊ 　 ｡*　　+　 　＊ 　･ ｡☆
-'
-# |::::::::::::::::::<<<stars
-EOF
+fi
